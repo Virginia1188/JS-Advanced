0ee94ff4fed4dbb21e9f75762a2f1a112bc1ffe1
Virginia1188 <113673043+Virginia1188@users.noreply.github.com>
2023-02-22 13:30:50 +0200

    Advanced Functions
    Lab Task

---
+++ Lesson 6 Advanced Functions/1.4. filterEmployees.js
@@ -0,0 +1,31 @@
+function filterEmployees(stringArr, criteria){
+    let employees = JSON.parse(stringArr);
+    let [prop, value] = criteria.split('-');
+
+    employees
+    .filter((x) => x[prop] == value)
+    .map((x, i) => `${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
+    .forEach(x => { console.log(x);});
+}
+filterEmployees(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`, 
+'gender-Female'
+);
